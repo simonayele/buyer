9d7bd0faa115aa2e887ff7198f8999ba1a3514ec
simonayele <simonayele@gmail.com>
2019-12-15 06:43:05 +0000

    Heroku deploy

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,6 +13,8 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  config.fog_public = false
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:

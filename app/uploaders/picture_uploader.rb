0c7e030412b4de5c15c0a1189949c6a45401de14
simonayele <simonayele@gmail.com>
2019-12-15 06:53:25 +0000

    AWS issue

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,7 +13,7 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  config.fog_public = false
+ 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
